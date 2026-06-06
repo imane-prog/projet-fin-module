c9b46c1065dd4cedff1dd317f8868ca910aba52f
imane-prog <imanerhanebou123@gmail.com>
2026-06-06 21:20:43 +0100

    Delete school directory

--- school/holidays/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HolidaysConfig(AppConfig):
-    name = 'holidays'
--- school/home_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomeAuthConfig(AppConfig):
-    name = 'home_auth'
